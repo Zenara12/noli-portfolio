9cc738f652fa0713373bf8c736525dff619ee844
Zenara12 <nolipernes@gmail.com>
2025-06-11 02:32:36 +0800

    update: intro summarize

--- src/components/IntroductionSection.vue
+++ src/components/IntroductionSection.vue
@@ -4,31 +4,16 @@
       <div class="column q-gutter-y-md">
         <div>
           <q-avatar size="15rem" :ratio="1" outline>
-            <q-img
-              :src="imgURL"
-              :ratio="1"
-              spinner-color="secondary"
-              loading="lazy"
-              class="border-2 border-info rounded-xl shadow-10"
-            />
+            <q-img :src="imgURL" :ratio="1" spinner-color="secondary" loading="lazy"
+              class="border-2 border-info rounded-xl shadow-10" />
           </q-avatar>
         </div>
         <div class="row q-gutter-sm flex items-center">
           <div v-for="(item, index) in contactInfo" :key="index">
-            <a
-              v-if="item.label !== 'Facebook' && item.label !== 'Phone'"
-              :href="item.link"
-              target="_blank"
-              class="text-grey-5 no-decoration"
-            >
-              <q-btn
-                :icon="item.icon"
-                :round="item.label !== 'Email' ? true : false"
-                :rounded="item.label === 'Email' ? true : false"
-                outline
-                color="secondary"
-                :label="item.label === 'Email' ? 'Email me' : ''"
-              />
+            <a :href="item.link" target="_blank" class="text-grey-5 no-decoration">
+              <q-btn :icon="item.icon" :round="item.label !== 'Email' ? true : false"
+                :rounded="item.label === 'Email' ? true : false" outline color="secondary"
+                :label="item.label === 'Email' ? 'Email me' : ''" />
             </a>
           </div>
         </div>
@@ -40,8 +25,8 @@
       </div>
       <p class="text-justify q-my-sm">
         I am a <span class="text-weight-bold text-secondary">web developer</span> with a passion for
-        building user-friendly websites and applications. Skilled in
-        <span class="text-weight-bold text-secondary">JavaScript, PHP, VueJS,</span> and
+        building user-friendly and mobile responsive web applications. Skilled in
+        <span class="text-weight-bold text-secondary">JavaScript, PHP, VueJS, React</span> and
         <span class="text-weight-bold text-secondary">database management,</span>
         I have worked on freelance projects that involve front-end development, backend integration,
         and troubleshooting. I am eager to learn, adapt, and grow in the field of web development,
@@ -60,6 +45,6 @@ const $q = useQuasar()
 const introClass = defineModel('introClass')
 const contactInfo = defineModel('contactInfo')
 
-const imgURL = 'images/profilepic.png'
+const imgURL = 'images/profilepic.jpg'
 const myName = 'Noli'
 </script>
